0e36bc195e8f91cb78917c11b7d960cc68bdc226
Michal <mic.rowinski@gmail.com>
2023-06-08 12:07:44 +0200

    revised pass validator

--- Java Script/3_walidator_hasła/main.js
+++ Java Script/3_walidator_hasła/main.js
@@ -6,21 +6,30 @@ const special = /[!@#$%^&*()]/
 const minValue = 10
 
 const showMsg = () => {
-	if (
-		pass.value.length >= minValue &&
-		pass.value.match(letters) &&
-		pass.value.match(numbers) &&
-		pass.value.match(special)
-	) {
-		p.textContent = 'masz zajebiste hasło'
+	if(pass.value.length > minValue &&
+		 pass.value.match(letters) && 
+		 pass.value.match(numbers)  && 
+		 pass.value.match(special)) {
+		p.textContent = 'masz bardzo dobre hasło!'
 		p.style.color = 'lime'
-	} else if (pass.value.length >= minValue && pass.value.match(letters) && pass.value.match(numbers)) {
+	} else if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)) {
 		p.textContent = 'masz dobre hasło'
-		p.style.color = 'yellow'
+		p.style.color = 'gold'
 	} else {
-		p.textContent = 'weź coś wpisz więcej ziomek bo żenada'
-		p.style.color = 'tomato'
+		p.textContent = 'masz słabe hasło'
+		p.style.color = ''
+		
 	}
 }
 
-pass.addEventListener('keyup', showMsg)
+
+const checkPass = () => {
+	if (pass.value !== '') {
+		showMsg()
+	} else {
+		p.textContent = 'nie podałeś hasła'
+		p.style.color = ''
+	}
+}
+
+pass.addEventListener('keyup', checkPass)
